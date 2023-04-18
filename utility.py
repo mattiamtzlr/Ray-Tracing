30e0bc91a43aa57a4e996aab395889bd77243d3e
Mattia Metzler <mattia.metzler@gmail.com>
2023-04-18 15:58:12 +0200

    consts, methods, etc.

---
+++ utility.py
@@ -0,0 +1,12 @@
+import math
+
+from vec3 import *
+from ray import *
+
+# constants
+INFINITY = float('inf')
+PI = 3.1415926535897932385
+
+# utility functions
+def degToRad(deg):
+    return deg * PI / 180
